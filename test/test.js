478d9020203f201693e721c5f005d13e68455f64
iarigby <marvinzem@gmail.com>
2020-03-28 12:18:30 +0400

    describe initial tests

--- test/test.js
+++ test/test.js
@@ -1,8 +1,8 @@
 const { expect } = require('chai')
 const rewire = require('rewire')
 
-describe('', () => {
-    it('', () => {
+describe('read the file and extract non exported function', () => {
+    it('referenceError for move should come up', () => {
         const submission = rewire('./files/simple.k')
         const main = submission.__get__('main')
         main()
