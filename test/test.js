f030cedcb79b27d4b7c457c4fbcc34632355d36a
iarigby <marvinzem@gmail.com>
2020-03-28 12:25:47 +0400

    fail the first attempt because of not understanding how modules work

--- test/test.js
+++ test/test.js
@@ -1,10 +1,14 @@
 const { expect } = require('chai')
+// https://stackoverflow.com/questions/14874208/how-to-access-and-test-an-internal-non-exports-function-in-a-node-js-module
 const rewire = require('rewire')
 
 describe('read the file and extract non exported function', () => {
     it('referenceError for move should come up', () => {
         const submission = rewire('./files/simple.k')
+        function move() {
+            return true
+        }
         const main = submission.__get__('main')
-        main()
+        expect(main()).equal(true)
     })
 })
