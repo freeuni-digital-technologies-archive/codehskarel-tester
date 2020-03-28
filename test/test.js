c5a8a2bca22b0137f623ac58b3a2a4291d12b25e
iarigby <marvinzem@gmail.com>
2020-03-28 12:37:38 +0400

    create simple class and first test case

--- test/test.js
+++ test/test.js
@@ -2,13 +2,22 @@ const { expect } = require('chai')
 // https://stackoverflow.com/questions/14874208/how-to-access-and-test-an-internal-non-exports-function-in-a-node-js-module
 const rewire = require('rewire')
 
+const TestClass = class {
+    constructor() {
+        this.i = 0
+        console.log(this)
+    }
+    move() {
+        this.i = this.i + 1
+    }
+}
 describe('read the file and extract non exported function', () => {
     it('console log should be called twice', () => {
         const submission = rewire('./files/simple.k')
         const main = submission.__get__('main')
-        function move() {
-            console.log('a')
-        }
-        submission.__set__('move', move)
+        const testClass = new TestClass()
+        submission.__set__('move', () => testClass.move())
+        main()
+        expect(testClass.i).equal(2)
     })
 })
