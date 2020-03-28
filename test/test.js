ef7e011787e4c08e29c4573c1ec4615c54d5ca7d
iarigby <marvinzem@gmail.com>
2020-03-28 12:26:23 +0400

    forget that the main function does not return anything

--- test/test.js
+++ test/test.js
@@ -5,10 +5,11 @@ const rewire = require('rewire')
 describe('read the file and extract non exported function', () => {
     it('referenceError for move should come up', () => {
         const submission = rewire('./files/simple.k')
+        const main = submission.__get__('main')
+        submission.__set__('move', move)
         function move() {
             return true
         }
-        const main = submission.__get__('main')
         expect(main()).equal(true)
     })
 })
