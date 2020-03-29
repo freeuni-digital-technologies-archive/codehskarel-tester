56784e3eea21ef9a09aa61db41f321c3d7f11d95
iarigby <marvinzem@gmail.com>
2020-03-29 23:29:22 +0400

    handle errors with reading the file

--- test/files/testCase.js
+++ test/files/testCase.js
@@ -1,30 +1,42 @@
 const { expect } = require('chai')
 const fileReader = require('../../src/karelFileReader')
-const { C } = require('../../src/Karel')
+// const fileName = '../test/files/broken.k'
 const fileName = '../test/files/integration.k'
+
 describe('integration file', () => {
     const config = {
         world: {
             beepers: [new C(1, 2)]
         }
     }
-    const { main, world, karel } = fileReader.setUpSubmission(fileName, config)
     try {
-        main()
-        // console.log(karel, world)
-        it('pick all beepers', () => {
-            expect(world.beepers.length).equal(0)
-        })
-        it('shoud be on 1x1 when it ends', () => {
-            expect(karel.position).eql(new C(2, 2))
-        })
-        it('fail test', () => {
-            expect(karel.position).eql(new C(2, 1))
-        })
+        const { main, world, karel } = fileReader.setUpSubmission(fileName, config)
+        try {
+            main()
+            // console.log(karel, world)
+            it('pick all beepers', () => {
+                expect(world.beepers.length).equal(0)
+            })
+            it('shoud be on 1x1 when it ends', () => {
+                expect(karel.position).eql(new C(2, 2))
+            })
+            it('fail test', () => {
+                expect(karel.position).eql(new C(2, 1))
+            })
+        } catch (err) {
+            it('There is a bug in the program', () => {
+                expect.fail(`error: ${err}, ${karel} `)
+            })
+        }
     } catch (err) {
-        it('There is a bug in the program', () => {
-            expect.fail(`error: ${err}, ${karel}`)
+        const errLine = err.stack.split('\n')[0]
+        const lineNumber = errLine.split(':')[1]
+        it('There is a problem with the file', () => {
+            if (err.name == "SyntaxError") {
+                expect.fail(`error reading the file on line ${lineNumber}`)
+            } else {
+                expect.fail(`Unknown error occured: ${errLine}, ${err.message}`)
+            }
         })
     }
-
 })
