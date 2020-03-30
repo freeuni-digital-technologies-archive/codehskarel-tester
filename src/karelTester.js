ecaf3c80c02581a24a18caef94ff231eb5cb6c17
iarigby <marvinzem@gmail.com>
2020-03-30 23:55:58 +0400

    rewrite tester, implement test files

--- src/karelTester.js
+++ src/karelTester.js
@@ -1,24 +1,60 @@
-const Mocha = require('mocha')
+const fileReader = require('./karelFileReader')
 
-module.exports.testFile = async function (testFile) {
-    const mocha = new Mocha({})
-    mocha.addFile(testFile)
-    const results = []
-    return new Promise((resolve, reject) => {
-        mocha
-            // .reporter('min')
-            .run()
-            .on('pass', test => results.push(test))
-            .on('fail', test => results.push(test))
-            .on('end', () => resolve(results.map(t => {
-                // console.log(t)
-                return {
-                    title: t.title,
-                    timedOut: t.timedOut,
-                    state: t.state,
-                    err: t.err
+function getFileReaderError(err) {
+    const result = {
+        error: true,
+        message: "there is a problem with the file",
+    }
+    const errLine = err.stack.split('\n')[0]
+    const lineNumber = errLine.split(':')[1]
+    if (err.name == "SyntaxError") {
+        result.details = `error reading the file on line ${lineNumber}`
+    } else {
+        result.details = `Unknown error occured: ${errLine}, ${err.message}`
+    }
+    return result
+}
+
+module.exports.KarelTester = class {
+    constructor(testFile) {
+        const { config, assertions } = require(testFile)
+        this.config = config
+        this.assertions = assertions
+    }
+
+    testSubmission(submissionFile) {
+        const results = []
+        try {
+            const { main, karel, world } = fileReader.setUpSubmission(submissionFile, this.config)
+            try {
+                main()
+                this.assertions.map(assertion => {
+                    try {
+                        const res = assertion(karel).__flags
+                        const result = {
+                            passed: true,
+                            message: res.message
+                        }
+                        results.push(result)
+                    } catch (err) {
+                        const result = {
+                            passed: false,
+                            message: err.message
+                        }
+                        results.push(result)
+                    } 
+                })
+            } catch (err) {
+                const result = {
+                    error: true,
+                    message: "there is a bug in the program",
+                    details: `${err}, ${karel}`,
                 }
-            })))
-    })
-
+                results.push(result)
+            }
+        } catch (err) {
+            results.push(getFileReaderError(err))
+        }
+        return results
+    }
 }
--- test/karelTester.test.js
+++ test/karelTester.test.js
@@ -1,11 +1,42 @@
+const { expect } = require('chai')
 
-const tester = require('../src/karelTester')
-const testFile = 'test/files/testCase.js'
-tester
-    .testFile(testFile)
-    .then(res => {
-        const passedCount = res.filter(e => e.state == 'passed').length
-        console.log(passedCount)
-        console.log(res.length)
-        console.log(res)
+const { KarelTester } = require('../src/karelTester')
+const path = `${process.cwd()}/test/files`
+
+describe('simple test case', () => {
+    const testFile = `${path}/simpleTest.js`
+    const tester = new KarelTester(testFile)
+    it('one test should run and it should pass', () => {
+        const [result] = tester.testSubmission(`${path}/simple.k`)
+        expect(result.passed).be.true
+        expect(result.message).eql("karel should go to 3x1")
     })
+    it('in case of a broken file it should report', () => {
+        const [result] = tester.testSubmission(`${path}/broken.k`)
+        expect(result.error).be.true
+        expect(result.message).eql("there is a problem with the file")
+    })
+})
+
+describe('integration test case', () => {
+    const testFile = `${path}/integrationTest.js`
+    const tester = new KarelTester(testFile)
+    it('should work with passing and failing tests', () => {
+        const results = tester.testSubmission(`${path}/integration.k`)
+        expect(results).length(3)
+        expect(results.filter(e => e.passed)).length(2)
+        const error = results.filter(e => !e.passed)[0]
+        expect(error.message).include('karel should finish on 2x2')
+    })
+})
+
+describe('buggy program', () => {
+    const testFile = `${path}/buggyTest.js`
+    const tester = new KarelTester(testFile)
+    it('should handle bugs in the program', () => {
+        const [ result ] = tester.testSubmission(`${path}/simple.k`)
+        expect(result.error).be.true
+        expect(result.message).eql("there is a bug in the program")
+        expect(result.details).contain('there is a wall in front of Karel')
+    })
+})
