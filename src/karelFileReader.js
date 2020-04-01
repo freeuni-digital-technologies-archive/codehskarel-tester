2ba3c38c678b9ffe3dda6fc74599bf842de0454d
iarigby <marvinzem@gmail.com>
2020-04-01 20:31:51 +0400

    rewrite regex match replacement, add return

--- src/karelFileReader.js
+++ src/karelFileReader.js
@@ -2,24 +2,40 @@
 const rewire = require('rewire')
 const { Karel, World, C } = require('./Karel')
 const fs = require('fs')
+
+const customStuctures = [
+    {
+        regex: /repeat\s*\((\s*[\d|\w]+\s*)\)/g,
+        replace: (match) => `for (let i=0; i < ${match[1]}; i++)`
+    },
+    {
+        regex: /return\(\)/g,
+        replace: match => `repeatFunction()`
+    }
+]
 function replaceCustomStructures(fileName) {
     let contents = fs.readFileSync(fileName, 'utf8')
-    const regex = /repeat\s*\((\s*[\d|\w]+\s*)\)/g
-    if (contents.match(regex) == null)
-        return fileName
     const newFile = fileName + '.fixed'
-    const matches = [...contents.matchAll(regex)]
-    matches.forEach(match => {
-        const replacement = `for (let i=0; i < ${match[1]}; i++)`
-        contents = contents.replace(match[0], replacement)
+    const replaced = customStuctures.map(structure => {
+        const regex = structure.regex
+        if (contents.match(regex) == null)
+            return false
+        const matches = [...contents.matchAll(regex)]
+        matches.forEach(match => {
+            const replacement = structure.replace(match)
+            contents = contents.replace(match[0], replacement)
+        })
+        return true
     })
+    if (!replaced.filter(e => e).length)
+        return fileName
     fs.writeFileSync(newFile, contents)
     return newFile
 }
 module.exports.setUpSubmission = (fileName, config = {}) => {
     const newFile = replaceCustomStructures(fileName)
     const submission = rewire(newFile)
-    const world = new World(config.world || {}) 
+    const world = new World(config.world || {})
     const karelConfig = config.karel || {}
     karelConfig.world = world
     const karel = new Karel(karelConfig)
